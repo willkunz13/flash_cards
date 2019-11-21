b68d6ea6c6e1872b88213fae2e0f679a69bb5af7
jerry <jerry@jerrys-MacBook-Pro.local>
2019-11-21 08:29:02 -0700

    Add code to round.rb

--- lib/round.rb
+++ lib/round.rb
@@ -1,26 +1,47 @@
 require_relative 'deck.rb'
 
 class Round
-	attr_reader :deck, :turns, :num_correct, :current_card, :percent_correct\
-			:percent_correct_by_category
+	attr_reader :deck, :turns, :num_correct
 
 	def initialize(d)
 		@deck = d
 		@turns = []
-		@num_correct = 0
-		@current_card = @deck.cards[0]
-		@percent_correct = @turns.count
-		@percent_correct_by_category = 
+		@correct = []
 	end
 
 	def take_turn(guess, card)
 		turn = Turn.new(guess, card)
 		@turns << turn
 		if turn.correct?
-			@num_correct ++
+			@correct << turn
 		end
 		@deck.cards.shift
 		return turn
+	end
+
+	def current_card
+		return @deck.cards[0]
+	end
+	
+	def number_correct
+		return @correct.count
+	end
+
+	def number_correct_by_category(weird)
+		cdeck = Deck.new(@correct)
+		return cdeck.cards_in_category(weird)
+	end
+	
+	def percent_correct
+		x = @correct.count
+		y = @turns.count
+		return x / y
+	end
+
+	def percent_correct_by_category(weird)	
+		x = number_correct_by_category(weird)
+		y = @correct.count
+		return x / y
 	end
 end
 
